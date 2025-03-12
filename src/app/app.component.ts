9f545f84d8e91f4ca1febf3731d37798e90ac7b4
art-pac <242816@edu.p.lodz.pl>
2025-03-12 23:11:35 +0100

    v0.0.2
    Application bundle generation failed. [5.673 seconds]
    
    X [ERROR] Could not resolve "node_modules/primeng/resources/themes/lara-light-blue/theme.css"
    
        angular:styles/global:styles:1:8:
          1 │ @import 'node_modules/primeng/resources/themes/lara-light-blue/them...
            ╵         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
      You can mark the path "node_modules/primeng/resources/themes/lara-light-blue/theme.css" as external to exclude it
    from the bundle, which will remove this error and leave the unresolved path in the bundle.
    
    X [ERROR] Could not resolve "node_modules/primeng/resources/primeng.min.css"
    
        angular:styles/global:styles:2:8:
          2 │ @import 'node_modules/primeng/resources/primeng.min.css';
            ╵         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
      You can mark the path "node_modules/primeng/resources/primeng.min.css" as external to exclude it from the bundle,
    which will remove this error and leave the unresolved path in the bundle.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { TextInputWithButtonComponent } from './text-input-with-button/text-input-with-button.component';
+import { ButtonDemo } from './button-demo/button-demo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [ButtonDemo]
 })
 export class AppComponent {
   title = 'angular-app';
