6ffd27141a114976985f0887cd6edccfccd56b65
art-pac <242816@edu.p.lodz.pl>
2025-03-20 19:03:29 +0100

    v1.0.0
    1st TASK done without styling

--- src/app/button-demo/button-demo.component.ts
+++ src/app/button-demo/button-demo.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 import { LoggerService } from '../services/logger.service';
+import { ButtonModule } from 'primeng/button'; // Upewnij się, że PrimeNG ButtonModule jest zaimportowany
 
 @Component({
   selector: 'button-demo',
   templateUrl: './button-demo.component.html',
   standalone: true,
-  imports: [ButtonModule]
+  imports: [ButtonModule]  // Zaimportowanie ButtonModule
 })
 export class ButtonDemo {
   @Input() userInput: string = ''; // Odbieranie wartości z InputTxtComponent
@@ -14,6 +14,6 @@ export class ButtonDemo {
   constructor(private loggerService: LoggerService) {}
 
   logValue(): void {
-    this.loggerService.logMessage(this.userInput);
+    this.loggerService.logMessage(this.userInput); // Logowanie wartości do konsoli
   }
 }
--- src/app/input-txt/input-txt.component.ts
+++ src/app/input-txt/input-txt.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
-import { ButtonDemo } from '../button-demo/button-demo.component';
+import { ButtonDemo } from '../button-demo/button-demo.component'; // Zaimportowanie ButtonDemo
 
 @Component({
   selector: 'app-input-txt',
   standalone: true,
-  imports: [InputTextModule, FloatLabelModule, FormsModule, ButtonDemo], // Import ButtonDemoComponent
+  imports: [InputTextModule, FloatLabelModule, FormsModule, ButtonDemo], // Zaimportowanie ButtonDemo
   templateUrl: './input-txt.component.html',
   styleUrls: ['./input-txt.component.css']
 })
 export class InputTxtComponent {
-  value3: string = ''; // Przechowuje wartość inputa
+  value3: string = ''; // Zmienna dla pola tekstowego
 }
